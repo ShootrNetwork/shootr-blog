9b5d6acb3f99dfebe476dd853686ad5d00efd645
antleocar <antonioleoncarrillo@gmail.com>
2017-01-19 18:22:36 +0100

    Update prod.secret.exs

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,12 +4,12 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :shootr_blog, ShootrBlog.Endpoint,
-  secret_key_base: "QqQz7Mx6Q+TDJsOLG5P7MCQgug8aqmq//fMRcs6TdB4HGVG3qOguDCBUglVW2sdv"
+  secret_key_base: ""
 
 # Configure your database
 config :shootr_blog, ShootrBlog.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "shootr_blog_prod",
+  username: "",
+  password: "",
+  database: "",
   pool_size: 20
